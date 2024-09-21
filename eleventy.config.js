079d6d2abb3cf839a0329eba0dba9afabdaecdc6
Natalie Weizenbaum <nweiz@google.com>
2024-09-21 15:40:19 -0700

    Improve the way embeds are rendered in the Atom feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -8,6 +8,7 @@ import yaml from "js-yaml";
 
 import componentsPlugin from "./helpers/components/index.js";
 import datesPlugin from "./helpers/dates.js";
+import feedPlugin from "./helpers/feed.js";
 import pagesPlugin from "./helpers/pages.js";
 import typePlugin from "./helpers/type.js";
 
@@ -15,6 +16,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPlugin(componentsPlugin);
   eleventyConfig.addPlugin(datesPlugin);
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  eleventyConfig.addPlugin(feedPlugin);
   eleventyConfig.addPlugin(pagesPlugin);
   eleventyConfig.addPlugin(typePlugin);
 
---
+++ helpers/feed.js
@@ -0,0 +1,68 @@
+import { JSDOM } from "jsdom";
+import escapeHtml from "escape-html";
+
+function adjustPostForFeed(post) {
+  const data = { ...post.data };
+  let url = post.url;
+  const container = JSDOM.fragment(
+    `<div>${post.templateContent}</div>`,
+  ).firstElementChild;
+
+  const embeds = container.querySelectorAll(".embed");
+  const replaceDataWithEmbed =
+    embeds.length === 1 &&
+    container.childElementCount === 1 &&
+    container.firstElementChild.classList.contains("embed");
+  for (const embed of embeds) {
+    let prose = embed.querySelector(".embed-prose").innerHTML;
+
+    const image = embed.querySelector(".embed-image");
+    if (image) {
+      if (image.classList.contains("embed-image-horizontal")) {
+        image.style.display = "block";
+        image.style.width = "100%";
+      } else {
+        image.style.float = "left";
+        image.style.maxWidth = "150px";
+      }
+    }
+
+    const title = embed.querySelector(".embed-title");
+    const link = embed.querySelector(".embed-link");
+    if (replaceDataWithEmbed) {
+      if (title) data.title = title.textContent;
+      if (link) url = link.href;
+      if (image) prose = image.outerHTML + " " + prose;
+    } else {
+      if (title) {
+        let titleHtml = "<h1>";
+        if (link) titleHtml += `<a href="${link.href}">`;
+        titleHtml += escapeHtml(title.textContent);
+        if (link) titleHtml += "</a>";
+        titleHtml += "</h1>";
+        prose = titleHtml + "\n" + prose;
+      }
+      if (image) prose = image.outerHTML + "\n" + prose;
+      prose = `
+        <div style="
+          padding: 0.75rem;
+          margin: 1rem 0 1.15rem;
+          border-radius: 0.5rem;
+          box-shadow:
+            0px 4px 5px rgba(0, 0, 0, 0.14),
+            0px 1px 10px rgba(0, 0, 0, 0.12),
+            0px 2px 4px rgba(0, 0, 0, 0.2);
+        ">${prose}</div>
+      `.replaceAll(/[ \n]+/g, " ");
+    }
+
+    const template = new JSDOM().window.document.createElement("template");
+    template.innerHTML = prose;
+    embed.replaceWith(template.content);
+  }
+  return { data, url, date: post.date, templateContent: container.innerHTML };
+}
+
+export default function feedPlugin(eleventyConfig) {
+  eleventyConfig.addLiquidFilter("adjustPostForFeed", adjustPostForFeed);
+}
