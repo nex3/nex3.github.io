3eeaf84446379f75961acde846f42813f987763e
Natalie Weizenbaum <nweiz@google.com>
2024-10-12 23:29:29 -0700

    Post

---
+++ source/blog/066-im-the-natalie-of.md
@@ -0,0 +1,20 @@
+{% genericPost "https://topposts.net/post/1728712880538/",
+    name: "I'm kind of the bcj of topposts.net",
+    time: "2024-10-12T06:01:20Z",
+    tags: "#bcj, #topposts.net (meta)",
+    author: "@topghost",
+    authorAvatar: "https://topposts.net/img/moon-dutiful.jpg",
+    reply: true %}
+{% endgenericPost %}
+
+{% genericPost "https://postnow.site/c26f903bcad646d3bdd8abc513d4c6c1.html",
+    name: "I see myself",
+    time: "2024-10-12T09:59:50-0300",
+    tags: "#@topghost",
+    author: "bcj",
+    inReplyUrl: "https://topposts.net/post/1728712880538/",
+    reply: true %}
+  <p>As the casey of postnow dot site</p>
+{% endgenericPost %}
+
+## I'm the Natalie of nex-3.com
--- tool/inject/h-entry.js
+++ tool/inject/h-entry.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 import { mf2 } from "microformats-parser";
 import * as prettier from "prettier";
 
@@ -18,17 +20,63 @@ function resolveUrl(url, base) {
   return new URL(url, base).toString();
 }
 
+/**
+ * Returns the h-entry in {@link items} with URL {@link urlToFind}.
+ *
+ * Returns null if there is no matching entry and throws an error if it's
+ * ambiguous.
+ */
+function findEntry(items, urlToFind, baseUrl) {
+  const candidates = items.filter(
+    (item) =>
+      item.type.includes("h-entry") &&
+      (item.properties.url ?? []).some(
+        (url) => resolveUrl(url, baseUrl) == urlToFind,
+      ),
+  );
+  if (candidates.length < 2) return candidates[0] ?? null;
+  throw new Error(
+    `URL ${url} has multiple top-level h-entries with matching URL`,
+  );
+}
+
+/**
+ * If {@link entry} is a reply, tries to create a tag for the post it's replying
+ * to.
+ *
+ * This builds one directly from the source URL if possible, from {@link items}
+ * if the original post embedded it, or else gives up and returns nothing.
+ */
+async function previousEntryToTag(entry, items, baseUrl) {
+  // TODO: de-duplicate replies so that if we fetch multiple pages that embed
+  // replies we don't include all of them.
+
+  const results = [];
+  for (const replyUrlUnresolved of entry.properties["in-reply-to"] ?? []) {
+    const replyUrl = resolveUrl(replyUrlUnresolved, baseUrl);
+    const response = await fetch(replyUrl);
+    const html = await response.text();
+    if (/\bh-entry\b/.test(html)) {
+      results.push(await hEntryToTag(html, replyUrl));
+      continue;
+    }
+
+    const replyEntry = findEntry(items, replyUrl, baseUrl);
+    if (replyEntry) {
+      results.push(parsedHEntryToTag(replyEntry, items, replyUrl, baseUrl));
+    }
+  }
+
+  return results.map(result => `${result}\n\n`).join('');
+}
+
 export async function hEntryToTag(html, url) {
   const { items } = mf2(html, { baseUrl: url.toString() });
+  return parsedHEntryToTag(findEntry(items, url, url), items, url, url);
+}
 
-  const entries = items.filter(
-    (item) => item.type.length === 1 && item.type[0] === "h-entry",
-  );
-  if (entries.length == 0) return null;
-  if (entries.length > 1) {
-    throw new Error(`URL ${url} has multiple top-level h-entries`);
-  }
-  const entry = entries[0];
+async function parsedHEntryToTag(entry, items, url, baseUrl) {
+  const previous = await previousEntryToTag(entry, items, baseUrl);
 
   const args = {};
   args.name = entry.properties.name?.[0];
@@ -43,7 +91,7 @@ export async function hEntryToTag(html, url) {
   }
   args.author =
     author?.properties?.nickname?.[0] ?? author?.properties?.name?.[0];
-  args.authorUrl = resolveUrl(author?.properties?.url?.[0], url);
+  args.authorUrl = resolveUrl(author?.properties?.url?.[0], baseUrl);
   args.authorAvatar =
     author?.properties?.photo?.[0] ?? author?.properties?.logo?.[0];
   if (typeof args.authorAvatar === "object") {
@@ -55,13 +103,13 @@ export async function hEntryToTag(html, url) {
   if (typeof inReplyTo === "string") {
     inReplyTo = { type: ["h-cite"], properties: { url: [inReplyTo] } };
   }
-  args.inReplyUrl = resolveUrl(inReplyTo?.properties?.url?.[0], url);
+  args.inReplyUrl = resolveUrl(inReplyTo?.properties?.url?.[0], baseUrl);
   args.inReplyName = inReplyTo?.properties?.name?.[0];
   args.inReplyAuthor = inReplyTo?.properties?.author?.[0];
 
-  return (
+  return previous + (
     "{% genericPost " +
-    JSON.stringify(resolveUrl(entry.properties.url?.[0], url) ?? url) +
+    JSON.stringify(resolveUrl(entry.properties.url?.[0], baseUrl) ?? url) +
     ",\n" +
     Object.entries(args)
       .filter(([_, value]) => value)
